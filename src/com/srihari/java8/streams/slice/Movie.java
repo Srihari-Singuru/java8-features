28b930d4a9e17b3291f0dec4a44d6490d4160ba7
Srihari Singuru <srihari.singuru@outlook.com>
2020-05-24 16:04:16 +0530

    Streams - Slicing example

---
+++ src/com/srihari/java8/streams/slice/Movie.java
@@ -0,0 +1,19 @@
+package com.srihari.java8.streams.slice;
+
+public class Movie {
+    private final String name;
+    private final int likes;
+
+    public Movie(String name, int likes) {
+        this.name = name;
+        this.likes = likes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+}
