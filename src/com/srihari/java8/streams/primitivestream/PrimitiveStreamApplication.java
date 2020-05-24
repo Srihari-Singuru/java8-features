ecaa9cc7712e53f569565be93b5e468cbc7ecc3d
Srihari Singuru <srihari.singuru@outlook.com>
2020-05-24 18:28:15 +0530

    Streams - Primitive streams

---
+++ src/com/srihari/java8/streams/primitivestream/PrimitiveStreamApplication.java
@@ -0,0 +1,14 @@
+package com.srihari.java8.streams.primitivestream;
+
+import java.util.stream.IntStream;
+
+/**
+ * Everything will be same as general Streams
+ */
+public class PrimitiveStreamApplication {
+    public static void main(String[] args) {
+        //IntStream.range(1, 5) // 5 is not included
+        IntStream.rangeClosed(1, 5)     // rangeClosed means, upperbound is inclusive
+                    .forEach(System.out::println);
+    }
+}
