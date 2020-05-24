7869480e9f15b6f5ca5ee06b57886941a399d216
Srihari Singuru <srihari.singuru@outlook.com>
2020-05-24 16:55:15 +0530

    Streams - Peeking elements

---
+++ src/com/srihari/java8/streams/peek/Movie.java
@@ -0,0 +1,19 @@
+package com.srihari.java8.streams.peek;
+
+public class Movie {
+    private final String name;
+    private final int price;
+
+    public Movie(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
