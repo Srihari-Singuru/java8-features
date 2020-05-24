d0a8a31d0643c13e222abb6b3435e87030dbffc0
Srihari Singuru <srihari.singuru@outlook.com>
2020-05-24 16:47:35 +0530

    Streams - Distinct elements

---
+++ src/com/srihari/java8/streams/distinct/SortingApplication.java
@@ -0,0 +1,21 @@
+package com.srihari.java8.streams.distinct;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class SortingApplication {
+    public static void main(String[] args) {
+        List<Movie> list = List.of(
+                new Movie("b", 10),
+                new Movie("d", 10),
+                new Movie("c", 15),
+                new Movie("a", 20)
+        );
+
+        // Display only distinct ticket prices
+        list.stream()
+                .map(Movie::getPrice)
+                .distinct()                     // to get distinct values
+                .forEach(System.out::println);
+    }
+}
